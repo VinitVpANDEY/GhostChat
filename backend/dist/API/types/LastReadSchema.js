a309fd8c24393a742d4238e50d5a790fae2dfea3
VinitVpANDEY <pvinitvinayak@gmail.com>
2025-03-09 11:50:01 +0530

    Removed dist folder from Git tracking

--- backend/dist/API/types/LastReadSchema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.lastReadSchema = void 0;
-const zod_1 = require("zod");
-exports.lastReadSchema = zod_1.z.object({
-    gId: zod_1.z.number(),
-    end: zod_1.z.number(),
-});
